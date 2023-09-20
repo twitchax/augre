982a7082561578d75f927976e365f5a662faa7d0
Aaron Roney <twitchax@gmail.com>
2023-09-19 22:29:42 -0700

    Prompt change.

--- src/services/gpt.rs
+++ src/services/gpt.rs
@@ -37,5 +37,10 @@ Please perform a code review of the following diff:
 
 {{diff}}
 
+Please prioritize the response by impact to the code, and please split the suggestions into three categories:
+1. Suggestions that pertain to likely runtime bugs or errors.
+2. Suggestions that pertain to likely logic bugs or errors.
+3. Suggestions that pertain to likely style bugs or errors.
 
+If possible, please also provide a suggested fix to the identified issue.  If you are unable to provide a suggested fix, please provide a reason why.
 "#;
